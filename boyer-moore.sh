220ba24f475addf48560e7c0a00ac6d7ccd0b8e1
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-11-03 10:27:41 +0530

    Add files via upload

---
+++ boyer-moore.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Hello world" 
+echo "Welcome to the Boyer Moore pattern matching algorithm"
+read -p "Enter yout name : " name
+echo "Hello $name, enter the text below :"
+read text
+echo "$name, please enter the pattern below :"
+read pattern
+
